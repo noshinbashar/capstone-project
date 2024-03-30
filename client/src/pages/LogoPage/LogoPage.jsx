f824f65457fbc2a48798ef3d4bcc12fddc01f284
noshinbashar <noshinbashar027@gmail.com>
2024-03-29 17:17:44 -0700

    made logo page responsive

--- client/src/pages/LogoPage/LogoPage.jsx
+++ client/src/pages/LogoPage/LogoPage.jsx
@@ -7,31 +7,32 @@ import { Link } from "react-router-dom";
 function LogoPage() {
     return (
         <>
-            <div>
+            <div className="logopage">
                 <div className="logoBox">
                     <div className="logoBox__container">
                         <img className="logoBox__image" src={Logo} alt="Logo" />
                         <p className="logoBox__text">Where Art Comes Alive</p>
                     </div>
+
                 </div>
 
-            </div>
 
+                <div className="imageBox">
+                    <div className="container">
+                        <img className="container__image-one" src={Image} alt="Image" />
+                        <img className="container__image-two" src={Canvas} alt="Image" />
+                    </div>
 
-            <div className="imageBox">
-                <div className="container">
-                    <img className="container__image-one" src={Image} alt="Image" />
-                    <img className="container__image-two" src={Canvas} alt="Image" />
                 </div>
 
-            </div>
-
-            <div>
-                <div className="logoButton">
-                    <Link to="/about">
-                        <button className="logoButton__btn">Learn More</button>
-                    </Link>
+                <div>
+                    <div className="logoButton">
+                        <Link to="/about">
+                            <button className="logoButton__btn">Learn More</button>
+                        </Link>
+                    </div>
                 </div>
+
             </div>
 
         </>
